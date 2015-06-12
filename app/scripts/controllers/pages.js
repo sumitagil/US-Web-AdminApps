0f5392f51d58f7bc1b81004b75aceca6870cc914
Sumita Panja <sumita.panja@globsyn.edu.in>
2015-06-12 13:49:06 +0530

    Update Search Section for Pagelist

--- app/scripts/controllers/pages.js
+++ app/scripts/controllers/pages.js
@@ -20,6 +20,19 @@ app.controller('pageCtrl', function ($scope, $http, $route) {
         $scope.pageSize_sub = 2;
         $scope.MaxPage_sub = 0;
         $scope.Run = 1;    
+	
+		 //Get All Project
+         $http.jsonp("http://beta.iservices.earlymoments.com/getprojectlist?token=741889E3-4565-40A1-982A-F15F7A923D72&format=json&callback=JSON_CALLBACK")
+         .success(function(data) {
+            $scope.allProjects = data.response;
+         }); //end
+	
+		//Get All Credit Rule
+         $http.jsonp("http://beta.iservices.earlymoments.com/getcreditruleslist?token=741889E3-4565-40A1-982A-F15F7A923D72&format=json&callback=JSON_CALLBACK")
+         .success(function(data) {
+            $scope.allCreditrules = data.response;
+         }); //end
+    
 
         //Get All data start
         $http.jsonp("http://beta.iservices.earlymoments.com/getcampaignlist?token=741889E3-4565-40A1-982A-F15F7A923D72&format=json&callback=JSON_CALLBACK")
